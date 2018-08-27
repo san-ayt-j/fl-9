9300f91e42be8f85fd4dceaefa2d2efbe9e97fe7
EC2 Default User <ec2-user@ip-172-31-14-203.us-east-2.compute.internal>
2018-08-27 13:30:54 +0000

    Added foles of homework. Completed homework

---
+++ FE_9_9_homework_data-types/homework/index.js
@@ -0,0 +1,73 @@
+function findType(param){
+    return typeof param;
+}
+
+function forEach(arr,func){
+    let outPut = [];
+    
+    for (let i=0; i<arr.length; i++){
+        outPut.push(func(arr[i]));
+    }
+    
+    return outPut;
+}
+
+function map(arr,func){
+    return forEach(arr,func);
+}
+
+function filter(arr,func){
+    let processed = forEach(arr,func);
+    let outPut=[];
+    
+    for (let i=0; i<processed.length; i++){
+        if (findType(processed[i]) !== 'undefined'){
+            outPut.push(processed[i]);
+        }
+    }
+    
+    return outPut;
+}
+
+function getAdultAppleLovers(data){
+    let adultAppleLovers = filter(data,function(el){
+       if (el.age > 18 && el.favoriteFruit === 'apple'){
+           return el.name;
+       } 
+    });
+    
+    return adultAppleLovers;
+}
+
+function keys(obj){
+    let outPut = [];
+    
+    for (let prop in obj){
+        if (obj.hasOwnProperty(prop)){
+            outPut.push(prop);    
+        }
+    }
+    
+    return outPut;
+}
+
+function values(obj){
+    let outPut = [];
+    
+    for (let prop in obj){
+        if (obj.hasOwnProperty(prop)){
+            outPut.push(obj[prop]);
+        }
+    }
+    
+    return outPut;
+}
+
+function showFormattedDate(date){
+    let dateDay = date.getDate();
+    let dateYear = date.getFullYear();
+    let monthArray = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    let shortMonth = monthArray[date.getMonth()];
+    
+    return 'It is ' + dateDay + ' of ' + shortMonth + ', ' + dateYear;
+}
