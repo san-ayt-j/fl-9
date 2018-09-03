16550842038e0bc3847b8eb10708ceceb34b6f79
EC2 Default User <ec2-user@ip-172-31-14-203.us-east-2.compute.internal>
2018-09-03 19:20:25 +0000

    Done main part of the task, without completed DragAndDrop function

--- FE_9_11_homework_dom/homework/src/app.js
+++ FE_9_11_homework_dom/homework/src/app.js
@@ -1,38 +1,91 @@
 //var rootNode = document.getElementById("root");
-function checkedItem() {
-    
+disableAddBtn(document.getElementById('action-input'));
+document.getElementById('action-input').setAttribute('onkeyup','disableAddBtn(this)');
+
+function checkedItem(aCheckbox) {
+    aCheckbox.innerText = 'check_box';
+}
+
+function deleteItem(aItem) {
+    aItem.style.display = 'none';
+}
+
+function disableAddBtn(aInput) {
+    let addNewActionBtn = document.getElementById('new-action-icon');
+    if (aInput.value === '') {
+        addNewActionBtn.style.color = '#D3D3D3';
+        addNewActionBtn.onclick = null;
+    } else {
+        addNewActionBtn.style.color = '#000';
+        addNewActionBtn.setAttribute('onClick', 'addNewAction()');
+    }
+}
+
+function allowDropItem(ev) {
+    console.log('allowDrop');
+    ev.preventDefault();
+}
+
+function dragItem(ev) {
+    console.log('drag');
+    ev.dataTransfer.setData('text', ev.target.id);
+}
+
+function dropItem(ev) {
+    console.log('drop');
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData('text');
+    ev.target.appendChild(document.getElementById(data));
 }
 
 function addNewAction() {
     var actionsListItems = document.getElementsByClassName('action-item');
-    if(actionsListItems.length == null || actionsListItems.length < 10) {
+    const actionListMaxLength = 10;
+    if(actionsListItems.length === null || actionsListItems.length < actionListMaxLength) {
         var actionItem = document.createElement('div');
         actionItem.setAttribute('class','action-item');
         actionItem.setAttribute('id','action-item');
         actionItem.style.display = 'flex';
+        actionItem.style.alignItems = 'center';
+        
+        actionItem.setAttribute('draggable','true');
+        actionItem.setAttribute('ondragstart','dragItem(event)');
+        actionItem.setAttribute('ondragover','allowDropItem(event)');
+        actionItem.setAttribute('ondrop','dropItem(event)');
         
         var actionItemCheckbox = document.createElement('i');
         actionItemCheckbox.setAttribute('class','material-icons');
         actionItemCheckbox.innerText = 'check_box_outline_blank';
+        actionItemCheckbox.style.flex = '5%';
+        actionItemCheckbox.style.cursor = 'pointer';
+        actionItemCheckbox.setAttribute('onClick','checkedItem(this)');
         
         actionItem.appendChild(actionItemCheckbox);
         
         var actionItemText = document.createElement('p');
-        actionItemText.innerText = 'input text';
-       
+        var inputText = document.getElementById('action-input').value;
+        var itemText = document.createTextNode(inputText);
+        actionItemText.appendChild(itemText);
         actionItem.appendChild(actionItemText);
+        document.getElementById('action-input').value = '';
+        disableAddBtn(document.getElementById('action-input'));
+       
+        actionItemText.style.flex = '90%';
+        actionItemText.style.margin = '0';
         
         var actionItemDelete = document.createElement('i');
         actionItemDelete.setAttribute('class','material-icons');
         actionItemDelete.innerText = 'delete';
+        actionItemDelete.style.cursor = 'pointer';
+        actionItemDelete.setAttribute('onClick','deleteItem(parentNode)');
        
         actionItem.appendChild(actionItemDelete);
+        actionItemDelete.style.flex = '5%';
         
         var actionsList = document.getElementById('actions-list');
         
         actionsList.appendChild(actionItem);
-    }
-    else {
+    } else {
         var newAction = document.getElementById('new-action');
         var newActionInput = document.getElementById('new-action-input');
         var newActionBtn = document.getElementById('new-action-btn');
